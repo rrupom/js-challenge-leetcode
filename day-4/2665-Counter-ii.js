ae923cd5019fe0dc6c559cb31a27318ca7011233
rrupom <rakib.t.rupom@gmail.com>
2023-09-08 22:13:06 +0600

    2665 Counter ||

---
+++ day-4/2665-Counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let copyInit = init;
+
+  return {
+    increment() {
+      copyInit++;
+      return copyInit;
+    },
+    decrement() {
+      copyInit--;
+      return copyInit;
+    },
+    reset() {
+      copyInit = init;
+      return copyInit;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
